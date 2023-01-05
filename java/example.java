e5042dc69a4da7d003e4c90fd326d2a3f37c0bcf
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-01-05 10:16:28 -0500

    Create example.java

---
+++ java/example.java
@@ -0,0 +1,9 @@
+// Use javac to compile
+public class myProgram {
+// Print method
+  static void print(String printEE) {
+    System.out.println(printEE);
+  }
+// End print method
+  print("Hello, World!");
+}
